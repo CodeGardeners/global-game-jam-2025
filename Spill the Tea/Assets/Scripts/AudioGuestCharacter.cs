ba360e3dfef4e9598027a50eba934fe6c9aff9fe
MarkusA <markus.auer@aurealize.com>
2025-01-24 23:35:24 +0100

    Audio Test Scene and first tests

---
+++ Spill the Tea/Assets/Scripts/AudioGuestCharacter.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace Audio
+{
+    [Serializable]
+    public class AudioGuestCharacter : MonoBehaviour
+    {
+
+        public bool seated = false;
+        
+        
+    
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+}
